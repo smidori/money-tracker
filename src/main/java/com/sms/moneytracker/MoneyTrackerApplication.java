3235199b7bbbb937c26575d7329c2b4b01139d51
smidori <smidori@gmail.com>
2023-08-30 00:55:51 +0100

    first commit

---
+++ src/main/java/com/sms/moneytracker/MoneyTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.sms.moneytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyTrackerApplication.class, args);
+	}
+
+}
